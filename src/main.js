37830a64a67b9ba1debc09dd31c15a8f2a0a78c2
JuliaKirik <juliakirichok9878@gmail.com>
2022-05-12 19:46:22 +0300

    Added

--- src/main.js
+++ src/main.js
@@ -1,24 +1,27 @@
-function Constructor(price, calories) {
+function Constructor(name, price, calories) {
+    this.name = name;
     this.price = price;
     this.calories = calories;
 }
 //розміри
-const SIZE_SMALL = new Constructor(50, 20);
-const SIZE_AVERAGE = new Constructor(75, 30);
-const SIZE_LARGE = new Constructor(100, 40);
+const SIZE_SMALL = new Constructor("Burger small", 50, 20);
+const SIZE_AVERAGE = new Constructor("Burger everage", 75, 30);
+const SIZE_LARGE = new Constructor("Burger large", 100, 40);
 //начинки
-const CHEESE = new Constructor(10, 20);
-const SALAD = new Constructor(20, 5);
-const POTATO = new Constructor(15, 10);
-const SPICE = new Constructor(15, 0);
-const TOPPING_MAYO  = new Constructor(20, 5);
+const CHEESE = new Constructor("Cheese", 10, 20);
+const SALAD = new Constructor("Salad", 20, 5);
+const POTATO = new Constructor("Potato", 15, 10);
+const SPICE = new Constructor("Spice", 15, 0);
+const TOPPING_MAYO  = new Constructor("Mayo", 20, 5);
 
 function Hamburger(size) {
     this.price = size.price;
     this.calories = size.calories;
+    this.modifiers = [];
     this.addModifier = function(modifier) {
         this.price += modifier.price;
         this.calories += modifier.calories;
+        this.modifiers.push (modifier);
     }
     this.getPrice = function() {
         return this.price;
